bdefb7fe8ddc8d005c1bb3694285e5880f4db509
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-05 00:49:31 -0600

    replaced sec group with module

--- ec2.tf
+++ ec2.tf
@@ -80,38 +80,51 @@ resource "aws_key_pair" "samba" {
   key_name   = var.key_name
   public_key = file("~/.ssh/id_rsa.pub")
   }
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
 
-resource "aws_security_group" "samba_file_share" {
-  name_prefix = "samba-file-share-"
+  name        = "Open VPN Security Group"
+  description = "vpn"
+  vpc_id      = data.aws_vpc.selected.id
+  egress_with_cidr_blocks = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = "0.0.0.0/0"
+      ipv6_cidr_blocks = "::/0"
+    },
+  ]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+     {
+      from_port   = 139
+      to_port     = 139
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "0.0.0.0/0"
+      }, {
+      from_port   = 445
+      to_port     = 445
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "0.0.0.0/0"
+      }, 
 
-  ingress {
-    from_port   = 139
-    to_port     = 139
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ]
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 445
-    to_port     = 445
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "samba-linux"
   }
 }
+
 
 output "public_ip" {
 value = module.ec2_instance.public_ip
